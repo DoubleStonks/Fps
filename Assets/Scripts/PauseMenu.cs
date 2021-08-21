6f418af33516e9124178fab17f85f57addd1a539
DoubleStonks <87582962+DoubleStonks@users.noreply.github.com>
2021-08-21 13:00:58 -0700

    Added pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool IsPause;
+    public GameObject pauseMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsPause)
+            {
+                Resume();
+            }
+
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        IsPause = false;
+        Time.timeScale = 1;
+    }
+
+    void Pause()
+    {
+        pauseMenu.SetActive(true);
+        IsPause = true;
+        Time.timeScale = 0;
+
+    }
+
+}
